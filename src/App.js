dc62cd5dd1ed386996c84c5dc610cd0d6969ddcd
Daniel Contreras <dantnadoriginal@gmail.com>
2022-10-19 00:52:38 -0500

    Created category items and categories components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import categories from "./categories";
+import Categories from "./components/categories/categories.component";
+
 const App = () => {
-  return "Hello world";
+  return (
+    <div className="app">
+      <Categories categories={categories} />
+    </div>
+  );
 };
 
 export default App;
