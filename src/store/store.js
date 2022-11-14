45c9f058c484105dddff0f5cfe72c88e51562ad0
Daniel Contreras <dantnadoriginal@gmail.com>
2022-11-13 18:52:10 -0600

    Added Redux Devtools for development

--- src/store/store.js
+++ src/store/store.js
@@ -2,8 +2,17 @@ import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [logger];
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 //Root reducer
 export const store = createStore(rootReducer, undefined, composedEnhancers);
